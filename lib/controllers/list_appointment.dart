21d1786353de2b77a4f281a42d0c13d2c24be0b0
ngyenlananh <23010823@st.phenikaa-uni.edu.vn>
2026-05-29 13:48:22 +0700

    Add files via upload

--- lib/controllers/list_appointment.dart
+++ lib/controllers/list_appointment.dart
@@ -1,7 +1,8 @@
 import '../models/appointment.dart';
+import 'generic_list.dart';
 
 class ListAppointment {
-  final List<Appointment> _appointments = [];
+  final GenericList<Appointment> _store = GenericList<Appointment>(idGetter: (a) => a.id);
 
   bool _isSameTime(DateTime date1, DateTime date2) {
     return date1.year == date2.year &&
@@ -11,43 +12,40 @@ class ListAppointment {
         date1.minute == date2.minute;
   }
 
-  // create
   bool createAppointment(Appointment appointment) {
-    if (_appointments.any((a) => a.id == appointment.id)) {
-      print("ID [${appointment.id}] đã tồn tại.");
-      return false;
-    }
-
     if (appointment.appointmentDate.isBefore(DateTime.now())) {
       print("Không thể đặt lịch trong quá khứ.");
       return false;
     }
 
-    bool isDuplicate = _appointments.any((a) =>
-        a.doctorId == appointment.doctorId &&
-        _isSameTime(a.appointmentDate, appointment.appointmentDate));
+    final all = _store.getAll();
+
+    if (all.any((a) => a.id == appointment.id)) {
+      print("ID [${appointment.id}] đã tồn tại.");
+      return false;
+    }
+
+    bool isDuplicate = all.any((a) =>
+        a.doctorId == appointment.doctorId && _isSameTime(a.appointmentDate, appointment.appointmentDate));
 
     if (isDuplicate) {
       print("Bác sĩ đã có lịch tại thời điểm này.");
       return false;
     }
 
-    _appointments.add(appointment);
-    print("Thêm lịch hẹn thành công!");
-    return true;
+    final ok = _store.add(appointment);
+    if (ok) {
+      print("Thêm lịch hẹn thành công!");
+      return true;
+    }
+    print("Thêm lịch hẹn thất bại.");
+    return false;
   }
 
-  //read
-  List<Appointment> getAllAppointments() {
-    return List.unmodifiable(_appointments);
-  }
+  List<Appointment> getAllAppointments() => _store.getAll();
 
-  Appointment? getById(String id) {
-    int index = _appointments.indexWhere((a) => a.id == id);
-    return index != -1 ? _appointments[index] : null;
-  }
+  Appointment? getById(String id) => _store.getById(id);
 
-  //update
   bool updateAppointment(
     String id, {
     DateTime? newDate,
@@ -55,11 +53,13 @@ class ListAppointment {
     String? newStatus,
     String? newNote,
   }) {
-    var appt = getById(id);
+    final appt = getById(id);
     if (appt == null) {
       print("Không tìm thấy lịch hẹn có ID [$id].");
       return false;
     }
+
+    final all = _store.getAll();
 
     if (newDate != null) {
       if (newDate.isBefore(DateTime.now())) {
@@ -67,47 +67,50 @@ class ListAppointment {
         return false;
       }
 
-      bool isDuplicate = _appointments.any((a) =>
+      bool isDuplicate = all.any((a) =>
           a.id != id && a.doctorId == appt.doctorId && _isSameTime(a.appointmentDate, newDate));
 
       if (isDuplicate) {
         print("Cập nhật thất bại. Bác sĩ đã có lịch tại thời điểm mới.");
         return false;
       }
-   
-      appt.appointmentDate = newDate;
     }
 
-    if (newReason != null) appt.reason = newReason;
-    if (newStatus != null) appt.status = newStatus;
-    if (newNote != null) appt.note = newNote;
+    final updated = _store.update(id, (a) {
+      if (newDate != null) a.appointmentDate = newDate;
+      if (newReason != null) a.reason = newReason;
+      if (newStatus != null) a.status = newStatus;
+      if (newNote != null) a.note = newNote;
+    });
+
+    if (!updated) {
+      print("Cập nhật thất bại.");
+      return false;
+    }
 
     print("Cập nhật lịch hẹn [$id] thành công");
     return true;
   }
 
-  //delete
   bool deleteAppointment(String id) {
-    int initialLength = _appointments.length;
-    _appointments.removeWhere((a) => a.id == id);
-
-    if (_appointments.length < initialLength) {
+    final ok = _store.delete(id);
+    if (ok) {
       print("Xóa lịch hẹn [$id] thành công!");
       return true;
-    } else {
-      print("Không tìm thấy lịch hẹn [$id] để xóa.");
-      return false;
     }
-    }
+    print("Không tìm thấy lịch hẹn [$id] để xóa.");
+    return false;
+  }
 
   void displayAll() {
-    if (_appointments.isEmpty) {
+    final all = _store.getAll();
+    if (all.isEmpty) {
       print("Danh sách lịch hẹn rỗng.");
       return;
     }
 
-    print("DANH SÁCH LỊCH HẸN (${_appointments.length}) ===");
-    for (var a in _appointments) {
+    print("DANH SÁCH LỊCH HẸN (${all.length}) ===");
+    for (var a in all) {
       a.displayInfo();
     }
   }
